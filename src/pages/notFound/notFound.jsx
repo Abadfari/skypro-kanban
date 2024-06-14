5c3e23f564ba6466e199c68a09cd04ad9fa3e84d
Abadfari <wildsloth.screw@gmail.com>
2024-06-14 14:36:04 +0300

    finish notFound styles

--- src/pages/notFound/notFound.jsx
+++ src/pages/notFound/notFound.jsx
@@ -2,11 +2,29 @@ import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
   return (
-    <div>
-      Такой страницы не существует...
-      <Link to="/">На главную</Link>
+    <div style={styles.container}>
+      <p style={styles.text}>Такой страницы не существует...</p>
+      <Link to="/" style={styles.link}>
+        На главную
+      </Link>
     </div>
   );
+};
+
+const styles = {
+  container: {
+    textAlign: "center",
+    marginTop: "50px",
+  },
+  text: {
+    fontSize: "20px",
+    marginBottom: "20px",
+  },
+  link: {
+    color: "blue",
+    textDecoration: "none",
+    fontWeight: "bold",
+  },
 };
 
 export default NotFoundPage;
